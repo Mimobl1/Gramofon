123b50dd4ce239469c0810acdc26b8e9649dee79
Mimobl1 <96305416+Mimobl1@users.noreply.github.com>
2026-09-23 05:38:06 +0200

    fix r2 sync

--- public/collection-data.js
+++ public/collection-data.js
@@ -3,7 +3,7 @@ window.VINYL_COLLECTION = [
     "id": "Vinyl_Collection_1969_-_THE_SOFT_PARADE",
     "name": "THE SOFT PARADE",
     "author": "1969",
-    "color": "#4b5f35",
+    "color": "#613847",
     "duration": "",
     "folder": "Vinyl Collection/1969 - THE SOFT PARADE",
     "cover": "",
